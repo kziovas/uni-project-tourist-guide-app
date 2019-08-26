cfcb547b054dc61b55040fe8d23384097c933c37
Kostas Ziovas <k_ziovas@yahoo.gr>
2019-08-26 21:10:02 +0300

    Begin

---
+++ FinalProject/RapidAppDev/RapidAppDev/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace RapidAppDev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.πΑΡΑΛΙΕΣToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.κΑΜΠΙΝΓΚToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.μΟΥΣΕΙΑToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.μΝΗΜΕΙΑToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.μΠΑΡToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.σΙΝΕΜΑToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.εΞΟΔΟToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.εΞΟΔΟΣToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem,
+            this.πΑΡΑΛΙΕΣToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem,
+            this.εΞΟΔΟToolStripMenuItem,
+            this.εΞΟΔΟΣToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1110, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(103, 26);
+            this.homeToolStripMenuItem.Text = "ΛΕΥΚΑΔΑ";
+            // 
+            // πΑΡΑΛΙΕΣToolStripMenuItem
+            // 
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem,
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem,
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem});
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.Name = "πΑΡΑΛΙΕΣToolStripMenuItem";
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.Size = new System.Drawing.Size(108, 26);
+            this.πΑΡΑΛΙΕΣToolStripMenuItem.Text = "ΠΑΡΑΛΙΕΣ";
+            // 
+            // δΙΑΣΚΕΔΑΣΗToolStripMenuItem
+            // 
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.μΠΑΡToolStripMenuItem,
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem,
+            this.σΙΝΕΜΑToolStripMenuItem,
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem});
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.Name = "δΙΑΣΚΕΔΑΣΗToolStripMenuItem";
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.δΙΑΣΚΕΔΑΣΗToolStripMenuItem.Text = "ΔΙΑΣΚΕΔΑΣΗ";
+            // 
+            // μΕΤΑΚΙΝΗΣΗToolStripMenuItem
+            // 
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem.Name = "μΕΤΑΚΙΝΗΣΗToolStripMenuItem";
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.μΕΤΑΚΙΝΗΣΗToolStripMenuItem.Text = "ΜΕΤΑΚΙΝΗΣΗ";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.μΟΥΣΕΙΑToolStripMenuItem,
+            this.μΝΗΜΕΙΑToolStripMenuItem});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.White;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(122, 26);
+            this.toolStripMenuItem1.Text = "ΑΞΙΟΘΕΑΤΑ";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem,
+            this.κΑΜΠΙΝΓΚToolStripMenuItem});
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem2.ForeColor = System.Drawing.Color.White;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(106, 26);
+            this.toolStripMenuItem2.Text = "ΔΙΑΜΟΝΗ";
+            // 
+            // ξΕΝΟΔΟΧΕΙΑToolStripMenuItem
+            // 
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem.Name = "ξΕΝΟΔΟΧΕΙΑToolStripMenuItem";
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.ξΕΝΟΔΟΧΕΙΑToolStripMenuItem.Text = "ΞΕΝΟΔΟΧΕΙΑ";
+            // 
+            // κΑΜΠΙΝΓΚToolStripMenuItem
+            // 
+            this.κΑΜΠΙΝΓΚToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.κΑΜΠΙΝΓΚToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.κΑΜΠΙΝΓΚToolStripMenuItem.Name = "κΑΜΠΙΝΓΚToolStripMenuItem";
+            this.κΑΜΠΙΝΓΚToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.κΑΜΠΙΝΓΚToolStripMenuItem.Text = "ΚΑΜΠΙΝΓΚ";
+            // 
+            // μΟΥΣΕΙΑToolStripMenuItem
+            // 
+            this.μΟΥΣΕΙΑToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.μΟΥΣΕΙΑToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.μΟΥΣΕΙΑToolStripMenuItem.Name = "μΟΥΣΕΙΑToolStripMenuItem";
+            this.μΟΥΣΕΙΑToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.μΟΥΣΕΙΑToolStripMenuItem.Text = "ΜΟΥΣΕΙΑ";
+            // 
+            // μΝΗΜΕΙΑToolStripMenuItem
+            // 
+            this.μΝΗΜΕΙΑToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.μΝΗΜΕΙΑToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.μΝΗΜΕΙΑToolStripMenuItem.Name = "μΝΗΜΕΙΑToolStripMenuItem";
+            this.μΝΗΜΕΙΑToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.μΝΗΜΕΙΑToolStripMenuItem.Text = "ΜΝΗΜΕΙΑ";
+            // 
+            // οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem
+            // 
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem.Name = "οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem";
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem.Text = "ΟΡΓΑΝΩΜΕΝΕΣ";
+            // 
+            // εΛΕΥΘΕΡΕΣToolStripMenuItem
+            // 
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem.Name = "εΛΕΥΘΕΡΕΣToolStripMenuItem";
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.εΛΕΥΘΕΡΕΣToolStripMenuItem.Text = "ΕΛΕΥΘΕΡΕΣ";
+            // 
+            // γΥΜΝΙΣΤΩΝToolStripMenuItem
+            // 
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem.Name = "γΥΜΝΙΣΤΩΝToolStripMenuItem";
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+            this.γΥΜΝΙΣΤΩΝToolStripMenuItem.Text = "ΓΥΜΝΙΣΤΩΝ";
+            // 
+            // μΠΑΡToolStripMenuItem
+            // 
+            this.μΠΑΡToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.μΠΑΡToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.μΠΑΡToolStripMenuItem.Name = "μΠΑΡToolStripMenuItem";
+            this.μΠΑΡToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.μΠΑΡToolStripMenuItem.Text = "ΜΠΑΡ";
+            // 
+            // εΣΤΙΑΤΟΡΙΑToolStripMenuItem
+            // 
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem.Name = "εΣΤΙΑΤΟΡΙΑToolStripMenuItem";
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.εΣΤΙΑΤΟΡΙΑToolStripMenuItem.Text = "ΕΣΤΙΑΤΟΡΙΑ";
+            // 
+            // σΙΝΕΜΑToolStripMenuItem
+            // 
+            this.σΙΝΕΜΑToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.σΙΝΕΜΑToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.σΙΝΕΜΑToolStripMenuItem.Name = "σΙΝΕΜΑToolStripMenuItem";
+            this.σΙΝΕΜΑToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.σΙΝΕΜΑToolStripMenuItem.Text = "ΣΙΝΕΜΑ";
+            // 
+            // εΚΔΗΛΩΣΕΙΣToolStripMenuItem
+            // 
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem.Name = "εΚΔΗΛΩΣΕΙΣToolStripMenuItem";
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.εΚΔΗΛΩΣΕΙΣToolStripMenuItem.Text = "ΕΚΔΗΛΩΣΕΙΣ";
+            // 
+            // εΞΟΔΟToolStripMenuItem
+            // 
+            this.εΞΟΔΟToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.εΞΟΔΟToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.εΞΟΔΟToolStripMenuItem.Name = "εΞΟΔΟToolStripMenuItem";
+            this.εΞΟΔΟToolStripMenuItem.Size = new System.Drawing.Size(125, 26);
+            this.εΞΟΔΟToolStripMenuItem.Text = "ΣΧΕΤΙΚΑ ΜΕ";
+            // 
+            // εΞΟΔΟΣToolStripMenuItem
+            // 
+            this.εΞΟΔΟΣToolStripMenuItem.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.εΞΟΔΟΣToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.εΞΟΔΟΣToolStripMenuItem.Margin = new System.Windows.Forms.Padding(175, 0, 0, 0);
+            this.εΞΟΔΟΣToolStripMenuItem.Name = "εΞΟΔΟΣToolStripMenuItem";
+            this.εΞΟΔΟΣToolStripMenuItem.Size = new System.Drawing.Size(91, 26);
+            this.εΞΟΔΟΣToolStripMenuItem.Text = "ΕΞΟΔΟΣ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem ξΕΝΟΔΟΧΕΙΑToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem κΑΜΠΙΝΓΚToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem μΟΥΣΕΙΑToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem μΝΗΜΕΙΑToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem πΑΡΑΛΙΕΣToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem οΡΓΑΝΩΜΕΝΕΣToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem εΛΕΥΘΕΡΕΣToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem γΥΜΝΙΣΤΩΝToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem δΙΑΣΚΕΔΑΣΗToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem μΠΑΡToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem εΣΤΙΑΤΟΡΙΑToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem σΙΝΕΜΑToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem μΕΤΑΚΙΝΗΣΗToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem εΚΔΗΛΩΣΕΙΣToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem εΞΟΔΟToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem εΞΟΔΟΣToolStripMenuItem;
+    }
+}
+
