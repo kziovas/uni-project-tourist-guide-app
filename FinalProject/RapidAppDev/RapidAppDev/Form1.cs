9ac9b4b2d0eafd0b4581bca44b4d13eafcbc9b78
Kostas Ziovas <k_ziovas@yahoo.gr>
2019-10-13 16:40:20 +0300

    Final version submitted

--- FinalProject/RapidAppDev/RapidAppDev/Form1.cs
+++ FinalProject/RapidAppDev/RapidAppDev/Form1.cs
@@ -12,9 +12,31 @@ namespace RapidAppDev
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+           
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- FinalProject/RapidAppDev/RapidAppDev/Properties/Settings.Designer.cs
+++ FinalProject/RapidAppDev/RapidAppDev/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RapidAppDev.Properties
-{
-
-
+namespace RapidAppDev.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\EventCalendar.mdb")]
+        public string EventCalendarConnectionString {
+            get {
+                return ((string)(this["EventCalendarConnectionString"]));
             }
         }
     }
